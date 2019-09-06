f7f4d4cf285683dac92ee35d484c31e42ec5ed5c
usha <usha@verdentum.org>
2019-09-06 16:46:56 +0530

    reactjs prgm

---
+++ index.js
@@ -0,0 +1,4 @@
+import React from "./dependencyies/react.development"
+import ReactDOM from "./dependencyies/react-dom.development"
+
+ReactDOM.render(<h1>Hello World!</h1>,document.getElementById("root"))
